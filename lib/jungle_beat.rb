bd86b93be5af9d4e2e61b9fc6296756929e31009
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2016-08-24 22:20:10 -0600

    implement play method

---
+++ lib/jungle_beat.rb
@@ -0,0 +1,25 @@
+require_relative 'linked_list'
+
+class JungleBeat
+
+  attr_reader :list
+  def initialize
+    @list = LinkedList.new
+  end
+
+  def append(data)
+    data.split(" ").map do |word|
+      list.append(word)
+    end
+    data
+  end
+
+  def count
+    list.count
+  end
+
+  def play
+    #incomplete
+  end
+
+end
